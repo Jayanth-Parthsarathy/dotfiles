6794d624cbdd2ee3ba9ecac5387df44c211e47a7
jayanth-parthsarathy <jparthaa@gmail.com>
2023-06-15 12:11:17 +0530

    Add some random changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
   alias h "history | sort | uniq | fzf | tr -d '\\n' | xclip -selection c"
   alias config '/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+  alias ls 'exa'
+  alias l 'exa -lbF --git'
+  alias ll 'exa -lbGF --git' 
+  alias la 'exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'
+  alias lx 'exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
 	starship init fish | source
 end
+fish_add_path /home/jayanth/.spicetify
